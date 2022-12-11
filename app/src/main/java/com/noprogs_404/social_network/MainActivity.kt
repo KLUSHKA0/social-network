8e2219fefdac344c11a84209ec1fecf25bbd7db0
KLUSHKA <afatar.slava@gmail.com>
2022-12-11 19:10:04 +0300

    first commit

---
+++ app/src/main/java/com/noprogs_404/social_network/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.noprogs_404.social_network
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.noprogs_404.social_network.ui.theme.SocialnetworkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SocialnetworkTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SocialnetworkTheme {
+        Greeting("Android")
+    }
+}
